01c7aa98918e6cae87aff2eb7b5f4ce248ec0448
Gabo B <gabo_banda@outlook.com>
2021-05-27 08:29:57 -0700

    Contraseñas encriptadas, centrar boton, respaldo de BD

--- Sistema_Convenios/CONTROLADOR/UsuarioControlador.cs
+++ Sistema_Convenios/CONTROLADOR/UsuarioControlador.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 using MODELO;
 
 namespace CONTROLADOR
 {
     public class UsuarioControlador
     {
+        public static byte[] GenerarSal()
+        {
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[16];
+            rngCsp.GetBytes(salt);
+            return salt;
+        }
+        public static byte[] EncriptarContraseña(string password, byte[] salt)
+        {
+            int iterations = 40000;
+            byte[] hash;
+
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, salt);
+            pbkdf2.IterationCount = iterations;
+            hash = pbkdf2.GetBytes(32);
+            return hash;
+        }
         public static void InsertarUsuario(USUARIO newUsuario)
         {
             try
@@ -18,6 +37,8 @@ namespace CONTROLADOR
                     && newUsuario.segundo_apellido != string.Empty && newUsuario.empresa != string.Empty
                     && UsuarioModelo.ExisteUsuario(newUsuario.email) != true)
                 {
+                    byte[] salt = GenerarSal();
+                    newUsuario.contrasena = Convert.ToBase64String(salt) + "!" + Convert.ToBase64String(EncriptarContraseña(newUsuario.contrasena, salt)).Substring(0, 20);
                     UsuarioModelo.InsertarUsuario(newUsuario);
                 }
                 else
@@ -58,12 +79,14 @@ namespace CONTROLADOR
         public static void ModificarUsuario(USUARIO usuarioModificado)
         {
             try
-            {   ///Verifica que sea un Usuario valido y que no esten vacios los campos requeridos
+            {   ///Verifica que sea un Usuario valido y que no esten vacios los campos reque    ridos
                 if (usuarioModificado.id_usuario > 0 && usuarioModificado.nombre != string.Empty
                     && usuarioModificado.primer_apellido != string.Empty && usuarioModificado.segundo_apellido != string.Empty
                     && usuarioModificado.email != string.Empty && usuarioModificado.empresa != string.Empty
                     && usuarioModificado.contrasena != string.Empty)
                 {
+                    byte[] salt = GenerarSal();
+                    usuarioModificado.contrasena = Convert.ToBase64String(salt) + "!" + Convert.ToBase64String(EncriptarContraseña(usuarioModificado.contrasena, salt)).Substring(0, 20);
                     UsuarioModelo.ModificarUsuario(usuarioModificado);
                 }
                 else
@@ -100,7 +123,14 @@ namespace CONTROLADOR
                     && usuario.contrasena != string.Empty)
                 {
                     var resultado = UsuarioModelo.IngresarSistema(usuario);
-                    if (resultado != null)
+
+                    string[] contraseña = resultado.contrasena.Split('!');
+
+                    byte[] salt = Convert.FromBase64String(contraseña[0]);
+                    byte[] hash = Convert.FromBase64String(contraseña[1]);
+                    byte[] newhash = EncriptarContraseña(usuario.contrasena, salt);
+
+                    if (hash.SequenceEqual(newhash))
                     {
                         return resultado;
                     }
---
+++ Sistema_Convenios/VISTA/Respaldo.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PagMaestra.Master" AutoEventWireup="true" CodeBehind="Respaldo.aspx.cs" Inherits="VISTA.Respaldo" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+  <script src="Include/js/Login.js"></script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Titulos" runat="server">
+  <h1 class="display-4 mb-2 mx-auto text-center" style="color:aliceblue">Respaldo de Base de Datos</h1>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="p-5">
+        <h3>¿Desea realizar un respaldo de la base de datos?</h3>
+        <asp:Button ID="btnRespaldo" Text="Descargar Respaldo" CssClass="btn btn-success" runat="server" OnClick="btnRespaldar_Click" />
+        <asp:Button ID="btnAtras" Text="Atras" CssClass="btn btn-primary" runat="server" OnClick="btnAtras_Click" />
+
+
+    </div>
+   
+</asp:Content>
